cc764965fa658bb50b639cb721a6736971d727ae
Kareem Nasrallah <kareemnasrallah88@gmail.com>
2024-06-24 13:05:31 +0300

    Make loading faster 3

--- New_Horizons.js
+++ New_Horizons.js
@@ -759,6 +759,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("showItem");
+    }else{
+      entry.target.classList.remove("showItem");
     }
   });
 });
